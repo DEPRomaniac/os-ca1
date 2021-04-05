61604654d1d090a06bbfa4b9d667a8ce04d7aae3
DEPRomaniac <50771091+DEPRomaniac@users.noreply.github.com>
2021-04-05 23:19:44 +0430

    Add files via upload

--- client.c
+++ client.c
@@ -1,45 +1,51 @@
 #include <netdb.h>
 #include <signal.h>
 #include <stdio.h> 
-#include <string.h>   //strlen 
+#include <string.h>   
 #include <stdlib.h> 
 #include <errno.h> 
-#include <unistd.h>   //close 
+#include <unistd.h>   
 #include <arpa/inet.h>
 #include <sys/types.h> 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <sys/time.h>
 
-#define MAX 80
+#define MAX 128
 // #define PORT 49000 
 #define SA struct sockaddr
-
+#define max_room_pop 5
 
 int times_up;
-int bid_sent = 0;
-int sockfd;
+int sockfd, sockfdToCli;
+struct sockaddr_in servaddr, cliaddr;
 
 void sig_handler(int signum){ 
   times_up = 1;
-  char no_bid[] = "no_bid\r\n";
-  if (!bid_sent)
-   {
-      printf("Times up! Press any key + enter to continue");
-      write(sockfd, no_bid, sizeof(no_bid));
-    }
 }
 
 void func()
 {
     char buff[MAX];
+    char msg[MAX];
     int n;
     int bidding_mode = 0;
     int project_id = -1;
+    int client_id;
+    int bid;
+    int end_bid_counter;
+    int my_turn;
+    int broadcast_project;
+    int broadcast_bid;
+    int turn;
+    int lowest_bid;
+    int bid_sent;
 
     bzero(buff, sizeof(buff));
     read(sockfd, buff, sizeof(buff));
     printf("From Server : %s", buff);
+    read(sockfd, &client_id, sizeof(int));
+    printf("Client ID is : %d\n", client_id);
     for (;;) {
         if (bidding_mode == 0){
             printf("Following projects are available:\n");
@@ -50,11 +56,11 @@ void func()
             bzero(buff, sizeof(buff));
             n = 0;
             scanf("%s", buff);
-            // while ((buff[n++] = getchar()) != '\n');
+            if((strncmp(buff, "exit", 4)) == 0){
+                printf("exitting bid1.00 ...\n");
+                break;
+            }
             project_id = atoi(buff);
-            // buff[n-1] = '\r';
-            // buff[n] = '\n';
-            // buff[n+1] = '\0';
             printf("requesting project %d bidding room from server...\n", project_id);
             write(sockfd, buff, sizeof(buff));
             bzero(buff, sizeof(buff));
@@ -65,45 +71,103 @@ void func()
                 bzero(buff, sizeof(buff));
                 read(sockfd, buff, sizeof(buff));
                 printf("From Server : %s", buff);
-                // printf("hereee\n");
                 if ((strncmp(buff, "Bidding has commenced", 21)) == 0){
                     bidding_mode = 1;
+                    lowest_bid = -1;
+                    end_bid_counter = 0;
+                    broadcast_project = project_id;
+                    broadcast_bid = -1;
+                    // printf("Waiting for recv 1\n");
+                    recv(sockfd, &turn, sizeof(int), 0);
+                    // printf("Waiting for recv 2\n");
+                    recv(sockfd, &my_turn, sizeof(int), 0);
                     printf("going to bid...\n");
-                    write(sockfd, "send list", sizeof("send list"));
                 }
             }
         }
         else if (bidding_mode == 1){
-            printf("awaiting your turn to bid...\n");
+            if(project_id == broadcast_project){
+                printf("awaiting your turn to bid...\n");
+                printf("%s\n", buff);
+                if((strncmp(buff, "Bidding has ended", 17)) == 0){
+                    bidding_mode = 0;
+                    write(sockfd, buff, sizeof(buff));
+                    continue;
+                }
+                if (broadcast_bid > 0 && (broadcast_bid < lowest_bid || lowest_bid < 1)){
+                        lowest_bid = broadcast_bid;
+                        end_bid_counter = 1;
+                }
+                else if (end_bid_counter > 0) end_bid_counter++;
+                if (lowest_bid > 0)
+                    printf("current lowest bid is %d\n", lowest_bid);
+                else
+                    printf("current lowest bid is None\n");
+
+                if ( turn != my_turn )
+                {
+                    printf("it is client %d's turn ----- lowest_bid is %d\n", turn, lowest_bid);
+                }
+                else
+                {
+                    // printf("hello! im bidding --- lowest_bid is %d\n", lowest_bid);
+                   bid_sent = 0;
+                   times_up = 0;
+                   alarm(10);
+                   printf("Enter your bid : ");
+                   bzero(buff, sizeof(buff));
+                   n = 0;
+                   scanf("%s", buff);
+                   printf("bid pending...\n");
+                   if (times_up == 0){
+                        alarm(0);
+                        bid_sent = 1;
+                        bid = atoi(buff);
+                        if (bid > 0 && (bid < lowest_bid || lowest_bid < 1))
+                        {
+                            end_bid_counter = 1;
+                            lowest_bid = bid;
+                            sprintf(msg, "Client %d has bidded new minimum bid : %d\r\n", client_id, bid);
+                        }
+                        else{
+                            if (end_bid_counter > 0) end_bid_counter++;
+                            sprintf(msg, "Client %d's bid : %s\r\n", client_id,buff);
+                        }
+                        
+                        turn++;
+                        printf("your bid is sent.\n");
+                   }
+                   else{
+                    if (!bid_sent)
+                    {
+                        if (end_bid_counter > 0) end_bid_counter++;
+                        bid = 0;
+                        printf("Times up!\n");
+                        sprintf(msg, "Client %d was unable to bid in time\r\n", client_id);
+                        turn++;
+                    }
+                   }
+                   turn = turn % max_room_pop;
+                   if(end_bid_counter >= max_room_pop)
+                        strcpy(msg, "Bidding has ended\r\n");
+                   sendto(sockfdToCli,&project_id,sizeof(int), 0,(struct sockaddr *)&cliaddr, sizeof cliaddr);
+                    sendto(sockfdToCli,msg,MAX, 0,(struct sockaddr *)&cliaddr, sizeof cliaddr);
+                    sendto(sockfdToCli,&turn,sizeof(int), 0,(struct sockaddr *)&cliaddr, sizeof cliaddr);
+                    sendto(sockfdToCli,&bid,sizeof(int), 0,(struct sockaddr *)&cliaddr, sizeof cliaddr);
+                }
+            
+            }
             bzero(buff, sizeof(buff));
-            read(sockfd, buff, sizeof(buff));
-            printf("From Server : %s", buff);
-            if ((strncmp(buff, "Your turn", 9)) == 0){
-               bid_sent = 0;
-               times_up = 0;
-               alarm(10);
-               printf("Enter your bid : ");
-               bzero(buff, sizeof(buff));
-               n = 0;
-               scanf("%s", buff);
-               // while ((buff[n++] = getchar()) != '\n' && times_up == 0);
-               // buff[n-1] = '\r';
-               // buff[n] = '\n';
-               // buff[n+1] = '\0';
-               printf("bid pending...\n");
-               if (times_up == 0){
-                bid_sent = 1;
-                write(sockfd, buff, sizeof(buff));
-                printf("your bid is sent.\n");
-                alarm(0);
-               }
-               else{
-               }
-            }
-            if ((strncmp(buff, "Bidding has ended", 17)) == 0){
-                bidding_mode = 0;
-                write(sockfd, buff, sizeof(buff));
-            }
+            // printf("waiting for recv broadcast_project\n");
+            recv(sockfdToCli, &broadcast_project, sizeof(int), 0);
+            // printf("waiting for recv buff\n");
+            recv(sockfdToCli, buff, sizeof(buff), 0);
+            // printf("waiting for recv turn\n");
+            recv(sockfdToCli, &turn, sizeof(turn), 0);
+            // printf("waiting for recv broadcast_bid\n");
+            recv(sockfdToCli, &broadcast_bid, sizeof(int), 0);
+            // printf("recv complete\n");
+
         }
     }
 }
@@ -112,9 +176,13 @@ int main(int argc , char *argv[])
 {
     int PORT = atoi(argv[1]);
     int connfd;
-    struct sockaddr_in servaddr, cli;
     
     signal(SIGALRM,sig_handler);
+    siginterrupt(SIGALRM,1);
+
+    int broadcast = 1;
+    int optim = 1;
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         printf("socket creation failed...\n");
@@ -122,6 +190,7 @@ int main(int argc , char *argv[])
     }
     else
         printf("Socket successfully created..\n");
+
     bzero(&servaddr, sizeof(servaddr));
   
     servaddr.sin_family = AF_INET;
@@ -135,6 +204,15 @@ int main(int argc , char *argv[])
     }
     else
         printf("connected to the server..\n");
+
+    sockfdToCli = socket(AF_INET,SOCK_DGRAM,0);
+    setsockopt(sockfdToCli,SOL_SOCKET,SO_BROADCAST,&broadcast,sizeof(broadcast));
+    setsockopt(sockfdToCli,SOL_SOCKET,SO_REUSEPORT,&optim,sizeof(optim));
+
+    cliaddr.sin_family = AF_INET;     
+    cliaddr.sin_port = htons(atoi(argv[1])); 
+    cliaddr.sin_addr.s_addr = inet_addr("255.255.255.255");
+    bind(sockfdToCli,(struct sockaddr *)&cliaddr,sizeof cliaddr);
   
     func();
   
--- server.c
+++ server.c
@@ -13,14 +13,14 @@
 #define TRUE   1 
 #define FALSE  0 
 // #define PORT 49000 
-#define max_clients 3
-#define max_room_pop 3
+#define max_clients 30
+#define max_room_pop 5
+#define MAX 128
 
 enum CLIENT_STATE{IDLE, REQ_PROJ, JOINED_ROOM, WAITING_TO_BID, BIDDING};
 
 void switch_turn(int proj, int num_of_projects, int* projects_room_client_turn, int* client_socket, int* projects_room_min_bid, int* client_req_proj){
-  char msg[64];
-  // for (int i =0; i < max_clients; i++){
+  char msg[MAX];
         int i = projects_room_client_turn[proj];
         int next_i = (i+1)%max_clients;
         for (int j = 0; j < max_clients; j++){
@@ -31,18 +31,11 @@ void switch_turn(int proj, int num_of_projects, int* projects_room_client_turn, 
         projects_room_client_turn[proj] = next_i;
         printf("client %d's turn\n", next_i );
         sprintf(msg, "Your turn : current lowest bid is %d\r\n", projects_room_min_bid[proj]);
-        // sprintf()
-        // send();
-        if (send(client_socket[next_i], msg, strlen(msg), 0) != strlen(msg) )
+        if (send(client_socket[next_i], msg, MAX, 0) != MAX )
             perror("send");                 
-    // }
-  // }
 }
 
 void sig_handler(int signum){ 
-  // times_up = 1;
-  // char *your_turn_msg = "Your turn \r\n";
-  // switch_turn();
   alarm(10);
 }
 
@@ -61,8 +54,6 @@ int main(int argc , char **argv)
     int PORT = atoi(argv[1]);
     int opt = TRUE;  
     int master_socket , addrlen , new_socket , activity, i , valread , sd;  
-    // int max_clients = 3 ,
-    // int client_req_proj[max_clients];
     int client_state[max_clients];
     for (int i = 0; i < max_clients; i++){
         client_req_proj[i] = -1;
@@ -79,38 +70,40 @@ int main(int argc , char **argv)
     // int projects_room_client_turn[] = {0, 0, 0, 0};
     int projects_avail[] = {1, 1, 1, 1};
 
-    char *list_of_projects = malloc(256);
+    char list_of_projects[MAX]; 
     bzero(list_of_projects, sizeof(list_of_projects));
          
-    char buffer[1025];
+    char buffer[MAX];
          
     fd_set readfds;  
          
-    char welcome_message[64];
-    char *start_bid_message = "Bidding has commenced \r\n";
-    char *end_bid_message = "Bidding has ended ";
-    char *req_acc_msg = "Request accepted \r\n";
-    char *req_dny_msg = "Request denied \r\n";
-    char *room_ready_msg = "Room is ready \r\n";
+    char welcome_message[MAX];
+    char start_bid_message[MAX];
+    strcpy(start_bid_message, "Bidding has commenced \r\n");
+    char end_bid_message[MAX];
+    strcpy(end_bid_message, "Bidding has ended ");
+    char req_acc_msg[MAX];
+    strcpy(req_acc_msg, "Request accepted \r\n");
+    char req_dny_msg[MAX];
+    strcpy(req_dny_msg, "Request denied \r\n");
+    char room_ready_msg[MAX];
+    strcpy(room_ready_msg, "Room is ready \r\n");
 
-    char my_str[64];
+    char my_str[MAX];
 
     signal(SIGALRM,sig_handler);
 
-    for (i = 0; i < max_clients; i++)  
-    {  
+    for (i = 0; i < max_clients; i++)  {  
         client_socket[i] = 0;  
     }  
          
-    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)  
-    {  
+    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)  {  
         perror("socket failed");  
         exit(EXIT_FAILURE);  
     }  
      
     if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, 
-          sizeof(opt)) < 0 )  
-    {  
+          sizeof(opt)) < 0 )  {  
         perror("setsockopt");  
         exit(EXIT_FAILURE);  
     }  
@@ -119,15 +112,13 @@ int main(int argc , char **argv)
     address.sin_addr.s_addr = INADDR_ANY;  
     address.sin_port = htons( PORT );  
          
-    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)  
-    {  
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)  {  
         perror("bind failed");  
         exit(EXIT_FAILURE);  
     }  
     printf("Listener on port %d \n", PORT);  
          
-    if (listen(master_socket, max_clients) < 0)  
-    {  
+    if (listen(master_socket, max_clients) < 0)  {  
         perror("listen");  
         exit(EXIT_FAILURE);  
     }  
@@ -184,17 +175,21 @@ int main(int argc , char **argv)
                     printf("Adding to list of sockets as %d\n" , i);  
                     printf("Number of clients is now %d\n" , num_of_clients);  
                     
-                    sprintf(welcome_message, "Welcome to bid1.00!\n Your Client ID is %d\r\n" , i);
-                    if( send(new_socket, welcome_message, strlen(welcome_message), 0) != strlen(welcome_message) )  {  
+                    sprintf(welcome_message, "Welcome to bid1.00! Your Client ID is %d\r\n" , i);
+                    if( send(new_socket, welcome_message, MAX, 0) != MAX )  {  
                         perror("send");  
                     } 
                     else puts("Welcome message sent successfully");  
-
+                    if( send(new_socket, &i, sizeof(i), 0) != sizeof(i) )  {  
+                        perror("send");  
+                    } 
+                    else puts("Client has recieved appointed Client ID");  
+                    
                     break;  
+
                 }  
             }
 
-            //send list of projects
             bzero(list_of_projects, sizeof(list_of_projects));
             strcpy(list_of_projects, "");
             for (i = 0; i < num_of_projects; i++){
@@ -202,23 +197,20 @@ int main(int argc , char **argv)
                     sprintf(my_str, "Project %d\n", projects[i]);
                     strcat(list_of_projects, my_str);
                 }
-            }
+            }        
             strcat(list_of_projects, "\r\n");
-            if( send(new_socket, list_of_projects, strlen(list_of_projects), 0) != strlen(list_of_projects) )  
+            if( send(new_socket, list_of_projects, MAX, 0) != MAX )  
             {  
                 perror("send");  
             }  
         }  
              
-        for (i = 0; i < max_clients; i++)  
-        {  
+        for (i = 0; i < max_clients; i++)  {  
             sd = client_socket[i];  
                  
-            if (FD_ISSET( sd , &readfds))  
-            {  
+            if (FD_ISSET( sd , &readfds))  {  
                 valread = read( sd , buffer, 1024);
-                if ( strcmp(buffer,"Exit") == 0 )  
-                {  
+                if ( strncmp(buffer,"exit",4) == 0 )  {  
                     getpeername(sd , (struct sockaddr*)&address , \
                         (socklen_t*)&addrlen);  
                     printf("Host disconnected , ip %s , port %d \n" , 
@@ -244,94 +236,64 @@ int main(int argc , char **argv)
                             printf("client %d is in room project %d\n", i, projects[requested_proj]);
                             client_req_proj[i] = requested_proj;
                             projects_room_pop[requested_proj] += 1;
-                            if (send(client_socket[i], req_acc_msg, strlen(req_acc_msg), 0) != strlen(req_acc_msg) )
+                            if (send(client_socket[i], req_acc_msg, MAX, 0) != MAX )
                                 perror("send");                 
                             
                             if (projects_room_pop[requested_proj] == max_room_pop){
                                 printf("begin bidding for project %d \n", requested_proj+1);
                                 projects_end_bid_counter[requested_proj] = 0;
-                                for(int j=max_clients-1; j >= 0; j--){
-                                // for(int j =0; j <max_clients; j++){
+                                
+                                for(int j =0; j <max_clients; j++){
+                                    if (client_req_proj[j] == requested_proj){
+                                        projects_room_client_turn[requested_proj] = j;
+                                        break;
+                                    }
+                                }
+                                int turn = projects_room_client_turn[requested_proj];
+                                int this_client_turn = 0;
+                                for(int j =0; j <max_clients; j++){
                                     if (client_req_proj[j] == requested_proj){
                                         client_state[j] = BIDDING;
                                         
-                                        if (send(client_socket[j], start_bid_message, strlen(start_bid_message), 0) != strlen(start_bid_message) )
+                                        
+                                        if (send(client_socket[j], start_bid_message, MAX, 0) != MAX )
                                             perror("send");   
+                                        if (send(client_socket[j], &turn, sizeof(turn), 0) != sizeof(turn) )
+                                            perror("send");   
+                                        if (send(client_socket[j], &this_client_turn, sizeof(int), 0) != sizeof(int) )
+                                            perror("send");   
+                                        printf("start bid message sent to Client %d\n",j);
+                                        this_client_turn++;
                                         projects_room_client_turn[requested_proj] = j;
                                         // alarm(10);
                                     }
                                 }
-                                int j = projects_room_client_turn[requested_proj];
-                                char msg[64];
-                                printf("client %d's turn\n", j );
-                                sprintf(msg, "Your turn : (current lowest bid is %d)\r\n", projects_room_min_bid[requested_proj]);
-                                // sprintf()
-                                // send();
-                                if (send(client_socket[j], msg, strlen(msg), 0) != strlen(msg) )
-                                    perror("send");                 
-                                break;
                             }
                         }
-
-
                     }
-                    else if (client_state[i] == BIDDING){
-                        int proj = client_req_proj[i];
-                        printf( "projects_room_client_turn[%d] = %d and i = %d\n", proj,projects_room_client_turn[proj], i);
-                        if (projects_room_client_turn[proj] == i){
-                            bid = atoi(buffer);
-                            projects_end_bid_counter[proj] += 1;
-                            if (bid > 0){
-                                if( projects_room_min_bid[proj] > bid )
-                                    projects_end_bid_counter[proj] = 1;
-                                    printf("new lowest bid for project %d: %ld for client %d\n", proj+1, bid, i);
-                                    projects_room_min_bid[proj] = bid;
-                                    projects_room_min_bid_client[proj] = i;
-                            }
-                            switch_turn(proj,num_of_projects, projects_room_client_turn, client_socket, projects_room_min_bid, client_req_proj);
-                        }
-                        else{
-                            printf("invalid bid\n");
-                            if (send(client_socket[i], req_dny_msg, strlen(req_dny_msg), 0) != strlen(req_dny_msg) )
-                                perror("send");                 
-                            // switch_turn(proj);
-                        }
-
-                        if ( projects_end_bid_counter[proj] == max_room_pop){
-                            char msg[64];
-                            printf("Bidding for project %d has ended\n", proj);
-                            printf("Client %d won the project. Congaratulations!", projects_room_min_bid_client[proj]);
-                            for (int j = 0; j < max_clients; j++){
-                                if (client_req_proj[j] == proj){
-                                    strcpy(msg, end_bid_message);
-                                    if (j == projects_room_min_bid_client[proj])
-                                        strcat(msg, "You won the project.\r\n");
-                                    else
-                                        strcat(msg, "You were outbidded.\r\n");
-                                    if (send(client_socket[j], msg, strlen(msg), 0) != strlen(msg) )
-                                        perror("send");                 
-                                    client_req_proj[j] = -1;
-                                    client_state[j] = IDLE;
-                                }
-                            }
+                    else if(client_state[i] == BIDDING){
+                        // printf("hello there\n");
+                        client_state[i] = REQ_PROJ;
+                        if((strncmp(buffer, "Bidding has ended", 17)) == 0){
+                            int proj = client_req_proj[i];
+                            client_state[i] = REQ_PROJ;
                             projects_avail[proj] = 0;
-                        }
-                    }
-                    else{
-                        bzero(list_of_projects, sizeof(list_of_projects));
-                        strcpy(list_of_projects, "");
+                            client_req_proj[i] = -1;
 
-                        for (i = 0; i < num_of_projects; i++){
-                            if(projects_avail[i] == 1){
-                                sprintf(my_str, "Project %d\n", projects[i]);
-                                strcat(list_of_projects, my_str);
-                            }
+                            strcpy(list_of_projects, "");
+                            for (int j = 0; j < num_of_projects; j++){
+                                if(projects_avail[j] == 1){
+                                    sprintf(my_str, "Project %d\n", projects[j]);
+                                    strcat(list_of_projects, my_str);
+                                }
+                            }        
+                            strcat(list_of_projects, "\r\n");
+                            if( send(client_socket[i], list_of_projects, MAX, 0) != MAX )  
+                            {  
+                                printf("fd is client_socket[%d] = %d\n" , i, client_socket[i]);
+                                perror("send");  
+                            }  
                         }
-                        strcat(list_of_projects, "\r\n");
-                        if( send(new_socket, list_of_projects, strlen(list_of_projects), 0) != strlen(list_of_projects) )  
-                        {  
-                            perror("send");  
-                        }  
                     }
                 }  
             }  
